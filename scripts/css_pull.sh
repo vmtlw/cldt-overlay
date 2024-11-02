ce15f83d8dcaf0672c10499d7b858a797b2ff7e6
vmtlw <kv@calculate.ru>
2024-11-02 21:58:23 +0300

    изменено:      profiles/CLDT/package.accept_keywords изменено:      scripts/css_pull.sh

--- scripts/css_pull.sh
+++ scripts/css_pull.sh
@@ -2,6 +2,7 @@
 shortname=${1:-cls}
 url=https://mirror.calculate-linux.org/release
 RELEASE=$(curl -s $url/ | grep -Eo 202.[0-9]...| sort -u | tail -1)
+filepath=${2:-"/var/calculate/linux/$shortname-$RELEASE-x86_64.iso"}
 wget -q --show-progress $url/$RELEASE/$shortname-$RELEASE-x86_64.iso $@
 
 
